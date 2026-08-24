17cfe06444976ae9ba00c7d3cb92e5d420f98964
Dyeel <kleptog0321@gmail.com>
2026-08-25 06:36:54 +0800

    feat: configure standalone fullscreen iOS web app meta tags to remove Safari browser bars when added to Home Screen

---
+++ src/app/+html.tsx
@@ -0,0 +1,25 @@
+import { ScrollViewStyleReset } from 'expo-router/html';
+import { type PropsWithChildren } from 'react';
+
+export default function Root({ children }: PropsWithChildren) {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no, viewport-fit=cover, user-scalable=no"
+        />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="apple-mobile-web-app-title" content="RepLog" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="theme-color" content="#07080B" />
+        <link rel="apple-touch-icon" href="/favicon.png" />
+        <ScrollViewStyleReset />
+      </head>
+      <body style={{ backgroundColor: '#07080B' }}>{children}</body>
+    </html>
+  );
+}
